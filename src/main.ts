532e2388d62b41fd631525f56fea703c07a3ec97
Tin Goricki <tingoricki1@protonmail.com>
2023-12-26 14:49:46 +0100

    Created a function that makes a 16x16 grid

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const etch = document.querySelector('.etch')
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function createGrid() {
+  for(let i = 0; i < 16; i++) {
+    for(let j = 0; j < 16; j++) {
+      const div = document.createElement('div')
+      div.classList.add("cell");
+      etch?.appendChild(div);
+    }
+  }
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+createGrid();
