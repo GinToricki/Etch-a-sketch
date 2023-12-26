8c0641acc89c62f2387c9a057b35a2b9dbfa7352
Tin Goricki <tingoricki1@protonmail.com>
2023-12-26 16:06:08 +0100

    Added rgb with fade

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 const etch = document.querySelector('.etch')
 const createNewGridBtn = document.querySelector('.create-new-grid-btn');
 let gridSize = 16;
+let color = "black";
+let num = [1,1,1]
+let maxRed = 280;
+let maxGreen = 280;
+let maxBlue = 280;
+let choice = 1;
 
 createNewGridBtn?.addEventListener('click', () => {
   deleteGrid();
@@ -15,8 +21,15 @@ function createGrid() {
     for(let j = 0; j < gridSize; j++) {
       const div = document.createElement('div')
       div.classList.add("cell");
-      div.style.cssText = `flex: 1 0 ${(1/gridSize)*100}%`
-      addEffect(div);
+      div.style.cssText = `flex: 1 0 ${(1/gridSize)*100}%`;
+      switch(choice) {
+        case 1 :
+          addEffect(div);
+          break;
+          default:
+            console.log("Error")
+            break;
+      }
       etch?.appendChild(div);
     }
   }
@@ -31,10 +44,31 @@ function deleteGrid() {
   }
 }
 
-function addEffect(div:Element) {
+function addEffectRGBFade(div:HTMLElement) {
   div.addEventListener('mouseover', () => {
-    div.classList.add("active");
+    div.style.cssText = `background-color: rgba(${Math.floor(Math.random()*changeMaxRed())} ${Math.floor(Math.random()* changeMaxGreen())} ${Math.floor(Math.random()* changeMaxBlue())}); flex: 1 0 ${(1/gridSize)*100}%`
   })
+}
+
+function addEffect(div:HTMLElement) {
+  div.addEventListener('mouseover', () => {
+    div.style.cssText = `background-color: ${color}; flex: 1 0 ${(1/gridSize)*100}%`
+  })
+}
+
+function changeMaxRed() {
+  maxRed = maxRed - 25
+  return maxRed;
+}
+
+function changeMaxGreen() {
+  maxGreen = maxGreen - 25
+  return maxGreen;
+}
+
+function changeMaxBlue() {
+  maxBlue = maxBlue - 25
+  return maxBlue;
 }
 
 createGrid();
