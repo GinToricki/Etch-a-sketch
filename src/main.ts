dd6ee33db0a7006fdbf4a5ddcf6883ba7b6133e7
Tin Goricki <tingoricki1@protonmail.com>
2023-12-26 15:12:03 +0100

    Made it so the user can choose the grid size

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,40 @@
 const etch = document.querySelector('.etch')
+const createNewGridBtn = document.querySelector('.create-new-grid-btn');
+let gridSize = 16;
+
+createNewGridBtn?.addEventListener('click', () => {
+  deleteGrid();
+  do {
+    gridSize = parseInt(prompt()!);
+  }while(gridSize < 0 || gridSize > 100)
+  createGrid();
+})
 
 function createGrid() {
-  for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
+  for(let i = 0; i < gridSize; i++) {
+    for(let j = 0; j < gridSize; j++) {
       const div = document.createElement('div')
       div.classList.add("cell");
+      div.style.cssText = `flex: 1 0 ${(1/gridSize)*100}%`
+      addEffect(div);
       etch?.appendChild(div);
     }
   }
+}
+
+function deleteGrid() {
+  for(let i = 0; i < gridSize; i++) {
+    for(let j = 0; j < gridSize; j++) {
+      const div = document.querySelector('.cell');
+      etch?.removeChild(div!);
+    }
+  }
+}
+
+function addEffect(div:Element) {
+  div.addEventListener('mouseover', () => {
+    div.classList.add("active");
+  })
 }
 
 createGrid();
